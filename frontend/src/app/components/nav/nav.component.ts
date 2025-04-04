46a891fe835042835127ec989479ffd5e83c26c8
rodis00 <happyekkov@gmail.com>
2025-04-04 14:49:22 +0200

    add nav component

---
+++ frontend/src/app/components/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  imports: [RouterLink],
+})
+export class NavComponent {
+  authService: AuthService = inject(AuthService);
+
+  handleClick() {
+    this.authService.logout();
+  }
+}
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  imports: [NavComponent],
 })
 export class HomeComponent {}
