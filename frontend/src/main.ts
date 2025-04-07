f5865700d8ce932eed26ce37af4bd89519e16976
rodis00 <happyekkov@gmail.com>
2025-04-07 15:33:03 +0200

    add polish number format, change line height in address

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,11 @@
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localePl, 'pl');
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
