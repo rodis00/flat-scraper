6409e83bf83dad4eff5e33803cd8d56093d01d98
rodis00 <happyekkov@gmail.com>
2025-04-09 17:40:28 +0200

    add new flat model

---
+++ src/main/java/com/example/flatscraper/model/Flat.java
@@ -0,0 +1,221 @@
+package com.example.flatscraper.model;
+
+public class Flat {
+    private String title;
+    private String url;
+    private String imageUrl;
+    private String price;
+    private String pricePerMeter;
+    private String address;
+    private String area;
+    private String rooms;
+    private String heating;
+    private String floor;
+    private String rent;
+    private String stateOfFinishing;
+    private String market;
+    private String formOfOwnership;
+    private String availableFrom;
+    private String advertiserType;
+    private String additionalInfo;
+    private String yearOfConstruction;
+    private String elevator;
+    private String buildingType;
+    private String buildingMaterial;
+
+    public String getBuildingMaterial() {
+        return buildingMaterial;
+    }
+
+    public void setBuildingMaterial(String buildingMaterial) {
+        this.buildingMaterial = buildingMaterial;
+    }
+
+    private String equipment;
+    private String security;
+    private String media;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPricePerMeter() {
+        return pricePerMeter;
+    }
+
+    public void setPricePerMeter(String pricePerMeter) {
+        this.pricePerMeter = pricePerMeter;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(String rooms) {
+        this.rooms = rooms;
+    }
+
+    public String getHeating() {
+        return heating;
+    }
+
+    public void setHeating(String heating) {
+        this.heating = heating;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getRent() {
+        return rent;
+    }
+
+    public void setRent(String rent) {
+        this.rent = rent;
+    }
+
+    public String getStateOfFinishing() {
+        return stateOfFinishing;
+    }
+
+    public void setStateOfFinishing(String stateOfFinishing) {
+        this.stateOfFinishing = stateOfFinishing;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+
+    public String getFormOfOwnership() {
+        return formOfOwnership;
+    }
+
+    public void setFormOfOwnership(String formOfOwnership) {
+        this.formOfOwnership = formOfOwnership;
+    }
+
+    public String getAvailableFrom() {
+        return availableFrom;
+    }
+
+    public void setAvailableFrom(String availableFrom) {
+        this.availableFrom = availableFrom;
+    }
+
+    public String getAdvertiserType() {
+        return advertiserType;
+    }
+
+    public void setAdvertiserType(String advertiserType) {
+        this.advertiserType = advertiserType;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+
+    public String getYearOfConstruction() {
+        return yearOfConstruction;
+    }
+
+    public void setYearOfConstruction(String yearOfConstruction) {
+        this.yearOfConstruction = yearOfConstruction;
+    }
+
+    public String getElevator() {
+        return elevator;
+    }
+
+    public void setElevator(String elevator) {
+        this.elevator = elevator;
+    }
+
+    public String getBuildingType() {
+        return buildingType;
+    }
+
+    public void setBuildingType(String buildingType) {
+        this.buildingType = buildingType;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getSecurity() {
+        return security;
+    }
+
+    public void setSecurity(String security) {
+        this.security = security;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+
+    public void setMedia(String media) {
+        this.media = media;
+    }
+}
